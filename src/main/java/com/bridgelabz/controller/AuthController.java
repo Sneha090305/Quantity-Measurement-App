c27067582f60dc5c67e9147a5bf35e54b2c45876
Sneha <snehangap@gmail.com>
2026-05-11 22:42:26 +0530

    [Sneha]: UC18: Implemented JWT Authentication and User Management

---
+++ src/main/java/com/bridgelabz/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.controller;
+
+import com.bridgelabz.dto.AuthRequest;
+import com.bridgelabz.dto.AuthResponse;
+import com.bridgelabz.dto.RegisterRequest;
+import com.bridgelabz.service.AuthService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(
+            @RequestBody RegisterRequest request) {
+
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(
+            @RequestBody AuthRequest request) {
+
+        return authService.login(request);
+    }
+}
